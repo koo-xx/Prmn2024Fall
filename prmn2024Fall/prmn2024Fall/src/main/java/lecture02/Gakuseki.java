2eda782027440242a07c3044ce629ae5ab72184a
koo-xx <b2231230@photon.chitose.ac.jp>
2024-11-03 20:41:21 +0900

    練習èª問題1

---
+++ prmn2024Fall/prmn2024Fall/src/main/java/lecture02/Gakuseki.java
@@ -0,0 +1,13 @@
+package lecture02;
+
+public class Gakuseki {
+
+    void hanbetu(String number, String gakuseki) {
+        if(number.equals(gakuseki)) {
+            System.out.println("complete!!");
+        }
+        else {
+            System.out.println("error!!");
+        }
+    }
+}
--- prmn2024Fall/prmn2024Fall/src/main/java/lecture02/prac1.java
+++ prmn2024Fall/prmn2024Fall/src/main/java/lecture02/prac1.java
@@ -6,6 +6,11 @@ public class prac1 {
 
         String number = "B2231230";
 
+        Scanner scan = new Scanner(System.in);
+        System.out.print("学籍番号を入力してください。：");
+        String gakuseki = scan.nextLine();
 
+        Gakuseki g = new Gakuseki();
+        g.hanbetu(number, gakuseki);
     }
 }
